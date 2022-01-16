7384dfcb3e016217d832462f0a793cd4ace6572a
stacksagar <mr.sagar.co@gmail.com>
2022-01-16 11:27:31 +0600

    changed

--- src/components/SingleProduct/index.jsx
+++ src/components/SingleProduct/index.jsx
@@ -2,17 +2,26 @@ import React, {useEffect, useState} from "react";
 import axiosInstance from "../../helpers/axiosInstance";
 import ProductDetails from "./ProductDetails";
 import ProductImages from "./ProductImages";
-
+import ScreenLoading from "../utilities/ScreenLoading";
 const SingleProduct = (props) => {
   const {productId} = props?.match.params;
   const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axiosInstance
       .get(`/api/product/${productId}`)
-      .then((res) => setProduct({...res.data?.product}))
+      .then((res) => {
+        setLoading(false);
+        setProduct({...res.data?.product});
+      })
       .catch(console.log);
   }, [productId]);
+
+  if (loading) {
+    return <ScreenLoading />;
+  }
 
   if (Object.values(product).length < 1) {
     return <div className="p-5 text-red-600">Product Not Found!</div>;
